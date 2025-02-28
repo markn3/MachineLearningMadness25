c9943d6e026d3e6bae0093ce009c985773117557
Mark Navalta <marknavalta.3@gmail.com>
2025-02-28 16:11:21 -0600

    Read all csvs and merge team names

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,36 @@
 import pandas as pd
+# Teams & Seasons
+m_teams = pd.read_csv("./data/MTeams.csv")
+w_teams = pd.read_csv("./data/WTeams.csv")
+m_seasons = pd.read_csv("./data/MSeasons.csv")
+w_seasons = pd.read_csv("./data/WSeasons.csv")
+
+# Tournament Seeds
+m_ncaa_seeds = pd.read_csv("./data/MNCAATourneySeeds.csv")
+w_ncaa_seeds = pd.read_csv("./data/WNCAATourneySeeds.csv")
+
+# Basic Game Results (Regular Season & Tournament)
+m_reg_compact = pd.read_csv("./data/MRegularSeasonCompactResults.csv")
+w_reg_compact = pd.read_csv("./data/WRegularSeasonCompactResults.csv")
+m_ncaa_compact = pd.read_csv("./data/MNCAATourneyCompactResults.csv")
+w_ncaa_compact = pd.read_csv("./data/WNCAATourneyCompactResults.csv")
+
+# Sample submission format (to guide matchup construction)
+sample_submission = pd.read_csv("./data/SampleSubmissionStage1.csv")
 
 
-def load_data():
-    data = pd.read_csv("./data/Cities.csv")
+# Data Preprocessing
 
-    return data
+# Merge winning team names into men's regular season compact results:
+m_reg_compact = m_reg_compact.merge(
+    m_teams[['TeamID', 'TeamName']], 
+    left_on='WTeamID', right_on='TeamID', 
+    how='left'
+).rename(columns={'TeamName': 'WTeamName'}).drop('TeamID', axis=1)
 
-
-if __name__ == "__main__":
-    data = load_data()
-    print(data)
+# Merge losing team names:
+m_reg_compact = m_reg_compact.merge(
+    m_teams[['TeamID', 'TeamName']], 
+    left_on='LTeamID', right_on='TeamID', 
+    how='left'
+).rename(columns={'TeamName': 'LTeamName'}).drop('TeamID', axis=1)
