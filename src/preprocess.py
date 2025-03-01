2e85e7fc27a0620e529089bce9e73234913b404d
Mark Navalta <marknavalta.3@gmail.com>
2025-03-01 13:51:01 -0600

    added ratings to main df and removed older seasons

--- src/preprocess.py
+++ src/preprocess.py
@@ -94,11 +94,52 @@ print(all_games['WTeamID'].nunique())
 
 # Offensive Rating
 
-# test
-all_games['WScore_diff'] = all_games['WScore'] - all_games['LScore']
-print(all_games)
+# Teams & Seasons
+m_reg_detailed = pd.read_csv("./data/MRegularSeasonDetailedResults.csv")
+m_t_detailed = pd.read_csv("./data/MNCAATourneyDetailedResults.csv")
 
-print((all_games['Season']<2003).sum())
+# Offensive Rating
+def calculate_ratings(df):
+    df['WPoss'] = df['WFGA'] - df['WOR'] + df['WTO'] + 0.44 * df['WFTA']
+    df['LPoss'] = df['LFGA'] - df['LOR'] + df['LTO'] + 0.44 * df['LFTA']
+
+    df['W_OffRtg'] = (df['WScore'] / df['WPoss']) * 100
+    df['L_OffRtg'] = (df['LScore'] / df['LPoss']) * 100
+
+    print(df)
+
+    # Defensive Rating
+    df['W_DefRtg'] = (df['LScore'] / df['LPoss']) * 100
+    df['L_DefRtg'] = (df['WScore'] / df['WPoss']) * 100
+
+
+    # Net Rating
+    df['W_NetRtg'] = df['W_OffRtg'] - df['W_DefRtg']
+    df['L_NetRtg'] = df['L_OffRtg'] - df['L_DefRtg']
+
+    # drop unnecessary columns
+    df = df[['Season', 'DayNum', 'WTeamID', 'LTeamID', 'W_OffRtg', 'L_OffRtg', 'W_DefRtg', 'L_DefRtg']]
+
+    return df
+
+m_reg_detailed = calculate_ratings(m_reg_detailed)
+m_t_detailed = calculate_ratings(m_t_detailed)
+
+m_detailed = pd.concat([m_reg_detailed, m_t_detailed], ignore_index=True)
+
+merged_final = pd.merge(
+    all_games, 
+    m_detailed, 
+    on=['Season', 'DayNum', 'WTeamID', 'LTeamID'], 
+    how='inner'
+)
+
+# Display a sample of the final merged dataset
+print("Merged Final Dataset Sample:")
+print(merged_final)
+
+# # Optionally, save the final merged dataset to a CSV
+# merged_final.to_csv("Merged_Final_Data.csv", index=False)
 
 # Detailed regular
 # [117748 rows x 8 columns]
--- src/test.py
+++ src/test.py
@@ -3,38 +3,41 @@ pd.set_option('display.max_columns', None)
  
 
 # Teams & Seasons
-m_teams = pd.read_csv("./data/MTeams.csv")  # Contains TeamID and TeamName
-# df = pd.read_csv("./data/MRegularSeasonDetailedResults.csv")
-df = pd.read_csv("./data/MNCAATourneyDetailedResults.csv")
-
-
-print(df)
-
+m_reg_detailed = pd.read_csv("./data/MRegularSeasonDetailedResults.csv")
+m_t_detailed = pd.read_csv("./data/MNCAATourneyDetailedResults.csv")
 
 # Offensive Rating
+def calculate_ratings(df):
+    df['WPoss'] = df['WFGA'] - df['WOR'] + df['WTO'] + 0.44 * df['WFTA']
+    df['LPoss'] = df['LFGA'] - df['LOR'] + df['LTO'] + 0.44 * df['LFTA']
 
-df['WPoss'] = df['WFGA'] - df['WOR'] + df['WTO'] + 0.44 * df['WFTA']
-df['LPoss'] = df['LFGA'] - df['LOR'] + df['LTO'] + 0.44 * df['LFTA']
+    df['W_OffRtg'] = (df['WScore'] / df['WPoss']) * 100
+    df['L_OffRtg'] = (df['LScore'] / df['LPoss']) * 100
 
-df['W_OffRtg'] = (df['WScore'] / df['WPoss']) * 100
-df['L_OffRtg'] = (df['LScore'] / df['LPoss']) * 100
+    print(df)
 
-print(df)
-
-
-# Defensive Rating
-df['W_DefRtg'] = (df['LScore'] / df['LPoss']) * 100
-df['L_DefRtg'] = (df['WScore'] / df['WPoss']) * 100
+    # Defensive Rating
+    df['W_DefRtg'] = (df['LScore'] / df['LPoss']) * 100
+    df['L_DefRtg'] = (df['WScore'] / df['WPoss']) * 100
 
 
-# Net Rating
-df['W_NetRtg'] = df['W_OffRtg'] - df['W_DefRtg']
-df['L_NetRtg'] = df['L_OffRtg'] - df['L_DefRtg']
+    # Net Rating
+    df['W_NetRtg'] = df['W_OffRtg'] - df['W_DefRtg']
+    df['L_NetRtg'] = df['L_OffRtg'] - df['L_DefRtg']
 
-# drop columns
+    # drop unnecessary columns
+    df = df[['Season', 'DayNum', 'WTeamID', 'LTeamID', 'W_OffRtg', 'L_OffRtg', 'W_DefRtg', 'L_DefRtg']]
 
-df = df[['Season', 'DayNum', 'WTeamID', 'LTeamID', 'W_OffRtg', 'L_OffRtg', 'W_DefRtg', 'L_DefRtg']]
+    return df
 
-print(df)
+m_reg_detailed = calculate_ratings(m_reg_detailed)
+m_t_detailed = calculate_ratings(m_t_detailed)
 
-all_games = pd.concat([m_regular_season_results, m_tourney_results], ignore_index=True)
+m_detailed = pd.concat([m_reg_detailed, m_t_detailed], ignore_index=True)
+
+# sort by Season, Team, and Game Day
+m_detailed.sort_values(by=['Season', 'TeamID', 'DayNum'], inplace=True)
+m_detailed.reset_index(drop=True, inplace=True)
+
+print(m_detailed)
+                       
