88a2e9ee523a453527d2253828c510577ba8c3aa
Mark Navalta <marknavalta.3@gmail.com>
2025-03-06 10:02:40 -0600

    seperate target from features

--- src/preprocess_final.py
+++ src/preprocess_final.py
@@ -10,10 +10,21 @@ women_df = pd.read_csv("./data/women_dataset.csv")
 # men_df['Gender'] = 'M'
 # women_df['Gender'] = 'W'
 
-combined_df = pd.concat([men_df, women_df], ignore_index=True)
+df = pd.concat([men_df, women_df], ignore_index=True)
 
-print(combined_df)
+# Seperate Target from features
+target = df['target']
+
+# We will drop columns that are identifiers if not needed as features
+# For example, Team1 and Team2 are matchup identifiers that might be used only for submission
+# However, if you already computed differential features (like net_diff), you can drop raw team IDs.
+features = df.drop(columns=['Target', 'Team1', 'Team2'])
+
+
+
+print(df)
 
 # One hot encode categorical features
 
-# ohe_df = pd.get_dummies(combined_df, columns=[''])
+ohe_df = pd.get_dummies(df, columns=['HomeCourt'])
+print(ohe_df)
