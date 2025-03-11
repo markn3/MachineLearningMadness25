2f2678eed037d4d37ef7540518ea1795ded0b9ac
Mark Navalta <marknavalta.3@gmail.com>
2025-03-11 18:08:47 -0500

    load dataset

--- src/pred_data.py
+++ src/pred_data.py
@@ -1,9 +1,7 @@
 import pandas as pd
-from itertools import combinations
-from autogluon.tabular import TabularPredictor
 
+m_df = pd.read_csv("./data/m_final.csv")  # Load mens dataframe
 
-
-m_df = pd.read_csv("./data/m_final.csv")  # Contains TeamID and TeamName
+print(m_df)
 
 
