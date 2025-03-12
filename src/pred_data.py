884926690e8f7c869e48727e49d0d79bc8b6388b
Mark Navalta <marknavalta.3@gmail.com>
2025-03-11 19:40:55 -0500

    Merge 2021 men and women data

--- src/pred_data.py
+++ src/pred_data.py
@@ -2,8 +2,7 @@ import pandas as pd
 import itertools
 
 
-m_df = pd.read_csv("./data/m_final_raw.csv")  # Load mens dataframe
-w_df = pd.read_csv("./data/w_final_raw.csv")
+
 
 
 s_df = pd.read_csv("./data/SampleSubmissionStage1.csv")  # Load mens dataframe
@@ -14,12 +13,27 @@ s_df[['Season', 'Team1', 'Team2']] = s_df['ID'].str.split('_', expand=True)
 # Convert to integers for easier comparison
 s_df[['Season', 'Team1', 'Team2']] = s_df[['Season', 'Team1', 'Team2']].astype(int)
 
+s_df.drop(columns=["Pred"], inplace=True)
+
 s_df = s_df[s_df['Season'] == 2021]
 print(s_df)
 
 # Get all unique teams for the season
-teams = pd.concat([s_df['Team1'], s_df['Team2']]).nunique()
-print(teams)
+num = pd.concat([s_df['Team1'], s_df['Team2']]).nunique()
+print(num)
+
 
 # New plan: Use the SampleSubmissionStage1 to get the team matches
 # Note: get IDs for men and women
+
+
+m_df = pd.read_csv("./data/m_final_raw.csv")  # Load mens dataframe
+w_df = pd.read_csv("./data/w_final_raw.csv") 
+
+
+m_df = m_df[m_df['Season']==2021]
+w_df = w_df[w_df['Season']==2021]
+all_games = pd.concat([m_df, w_df], ignore_index=True)
+
+print(all_games)
+
