8415cc5cf8cb9fbc73e40f67a1362cd0d9a39c7f
Mark Navalta <marknavalta.3@gmail.com>
2025-03-18 17:21:33 -0500

    Fixed submissions style

--- src/prediction.py
+++ src/prediction.py
@@ -4,36 +4,19 @@ from autogluon.tabular import TabularPredictor
 # Load your trained model (if not already loaded)
 
 m_predictor = TabularPredictor.load("./data/AutogluonModels/m_pred/")
-print(m_predictor)
 
-df = pd.read_csv("./data/m_matchups_1_4.csv")
-print(df)
+m_matches = pd.read_csv("./data/m_matchups_1_4.csv")
 # Get the predicted probabilities for the positive class (Team1 wins)
-df.rename(columns={"TeamA": "Team1", "TeamB":"Team2"}, inplace=True)
-predictions = m_predictor.predict_proba(df)
-print(predictions)
+m_matches.rename(columns={"TeamA": "Team1", "TeamB":"Team2"}, inplace=True)
+m_predictions = m_predictor.predict_proba(m_matches)
 
-# # Typically, predictions will be a DataFrame with columns for each class.
-# # If your target is binary (0 and 1) and you've set up your predictor such that 1 = Team1 wins,
-# # then you might extract the column for class 1:
-# matchup_features_df["Pred"] = predictions[1]
 
-# # Now, you can create the final submission file with the ID and Pred columns.
-# submission = matchup_features_df[["ID", "Pred"]]
+m_pred = m_matches[['matchup']].copy()
+m_pred.rename({"matchup": "ID"}, inplace=True)
+m_pred['Pred'] = m_predictions.iloc[:, 0]  # Use iloc for column selection
+print(m_pred)
+
 # submission.to_csv("submission.csv", index=False)
-
-
-
-
-
-
-# # Get the predicted probabilities for the positive class (Team1 wins)
-# predictions = m_predictor.predict_proba(matchup_features_df)
-
-# # Typically, predictions will be a DataFrame with columns for each class.
-# # If your target is binary (0 and 1) and you've set up your predictor such that 1 = Team1 wins,
-# # then you might extract the column for class 1:
-# matchup_features_df["Pred"] = predictions[1]
 
 # # Now, you can create the final submission file with the ID and Pred columns.
 # submission = matchup_features_df[["ID", "Pred"]]
